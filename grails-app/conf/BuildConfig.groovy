152f88efb376aedd3668d6be4437994c94cdf382
Jesus Federico <jesus@123it.ca>
2014-02-19 17:41:24 -0500

    Removed endpoint property. Now it is received as parameter

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,8 +33,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
+        //build(":tomcat:$grailsVersion",
+        build(":release:2.2.1",
               ":rest-client-builder:1.0.3") {
             export = false
         }
--- grails-app/services/com/ambasadoro/AmbasadoroService.groovy
+++ grails-app/services/com/ambasadoro/AmbasadoroService.groovy
@@ -1,7 +1,6 @@
 package com.ambasadoro
 
 class AmbasadoroService {
-    def endpoint = [:]
 
     def logParameters(Object params) {
         log.debug "----------------------------------"
@@ -9,19 +8,10 @@ class AmbasadoroService {
         log.debug "----------------------------------"
     }
 
-    def setEndpoint(endpoint){
-        this.endpoint = endpoint
-    }
+    def getCartridgeXML(ambasadoro, engineClass, endpoint = [:]) {
+        def http_endpoint = "http://" + endpoint.get("server") + "/" + endpoint.get("app")
+        def https_endpoint = "https://" + endpoint.get("server") + "/" + endpoint.get("app")
 
-    def retrieveEndpoint() {
-        return retrieveEndpoint("http")
-    }
-
-    def retrieveEndpoint(protocol) {
-        return protocol + "://" + endpoint.get("server") + "/" + endpoint.get("app")
-    }
-
-    def getCartridgeXML(ambasadoro, engineClass) {
         def cartridge = '' +
                 '<?xml version="1.0" encoding="UTF-8"?>' +
                 '<cartridge_basiclti_link xmlns="http://www.imsglobal.org/xsd/imslticc_v1p0"' +
@@ -35,10 +25,10 @@ class AmbasadoroService {
                 '                             http://www.imsglobal.org/xsd/imslticp_v1p0 http://www.imsglobal.org/xsd/lti/ltiv1p0/imslticp_v1p0.xsd">' +
                 '    <blti:title>' + ambasadoro.getTpTitle() + '</blti:title>' +
                 '    <blti:description>' + ambasadoro.getTpDescription() + '</blti:description>' +
-                '    <blti:launch_url>' + retrieveEndpoint() + '/' + endpoint.get("controller") + '/' + endpoint.get("action") + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '</blti:launch_url>' +
-                '    <blti:secure_launch_url>' + retrieveEndpoint('https') + '/' + endpoint.get("controller") + '/' + endpoint.get("action") + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '</blti:secure_launch_url>' +
-                '    <blti:icon>' + retrieveEndpoint() + '/images/' + (ambasadoro.getId()? ambasadoro.getId() + '/': '') + 'favicon.ico</blti:icon>' +
-                '    <blti:secure_icon>' + retrieveEndpoint('https') + '/images' + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '/favicon.ico</blti:secure_icon>' +
+                '    <blti:launch_url>' + http_endpoint + '/' + endpoint.get("controller") + '/' + endpoint.get("action") + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '</blti:launch_url>' +
+                '    <blti:secure_launch_url>' + https_endpoint + '/' + endpoint.get("controller") + '/' + endpoint.get("action") + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '</blti:secure_launch_url>' +
+                '    <blti:icon>' + http_endpoint + '/images/' + (ambasadoro.getId()? ambasadoro.getId() + '/': '') + 'favicon.ico</blti:icon>' +
+                '    <blti:secure_icon>' + https_endpoint + '/images' + (ambasadoro.getId()? '/' + ambasadoro.getId(): '') + '/favicon.ico</blti:secure_icon>' +
                 '    <blti:vendor>' +
                 '        <lticp:code>' + ambasadoro.getTpVendorCode() + '</lticp:code>' +
                 '        <lticp:name>' + engineClass.TP_VENDOR_NAME + '</lticp:name>' +
